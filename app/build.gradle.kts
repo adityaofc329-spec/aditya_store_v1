5795da9d51877b5d6a67dfa2366ec4f0c5e7f63b
adityaofc329-spec <adityaofc329@gmail.com>
2026-09-24 20:56:32 +0530

    Fix Android API 36 build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.adistore.v1"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.adistore.v1"
         minSdk = 26
